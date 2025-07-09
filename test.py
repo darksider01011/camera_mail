f6f43047c75143338c1c9679e2e1ba6041c0fd6f
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2025-07-09 16:34:41 +0330

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from datetime import date
 import requests
 import subprocess
 import smtplib
+import shutil
+import warnings
 import email
 import imaplib
 from time import sleep
@@ -12,6 +14,9 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 username = '' #Username(sender email address) EX: abcd@gmail.com
 password = '' #Gmail App Password EX: aswd dedw frfr frgt
@@ -19,12 +24,9 @@ target_email = "darksideofmosy@gmail.com"
 re_server = 'imap.gmail.com'
 
 def func():
-    i = 0
-    ii = int(i) 
     while True:
-        ii += 1
         try:
-            print(ii,"Retrieving Email Inbox...")
+            print("Retrieving Email Inbox...")
             print("")
 
             # main loop delay
@@ -33,7 +35,7 @@ def func():
             try:
                 mail = imaplib.IMAP4_SSL(re_server)
             except Exception as e:
-                print("Error: failed to start IMAP server")
+                print("ERROR: Failed to start IMAP server")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "failed to start imap server"
                 msg['From'] = username
@@ -44,7 +46,7 @@ def func():
                 <body>
                 <h1>failed to start imap server</h1>
                 <br>
-                <p>Error: {e}</p>
+                <p>ERROR: {e}</p>
                 </body>
                 </html>""".format(e = e)
 
@@ -59,14 +61,14 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Error Sent")
+                    print("INFO: Error Sent")
                 except:
-                    print("Error: Failed to send email")
+                    print("ERROR: Failed to send email")
 
             try:
                 mail.login(username, password)
             except Exception as e:
-                print("Error: failed to login to email")
+                print("ERROR: Failed to login to email")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "failed to login to email"
                 msg['From'] = username
@@ -75,9 +77,9 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>failed to login to email</h1>
+                <h1>Failed to login to email</h1>
                 <br>
-                <p>Error: {e}</p>
+                <p>ERROR: {e}</p>
                 </body>
                 </html>""".format(e = e)
 
@@ -92,24 +94,24 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Error  Sent")
+                    print("INFO: Error Sent")
                 except:
-                    print("Error: Failed to send email")
+                    print("ERROR: Failed to send email")
 
             try:
                 # choose mailbox
                 mail.select('inbox')
             except Exception as e:
-                print("Error: failed to select mail inbox")
+                print("ERROR: failed to select mail inbox")
                 msg = MIMEMultipart('alternative')
-                msg['Subject'] = "failed to select mail inbox"
+                msg['Subject'] = "Failed to select mail inbox"
                 msg['From'] = username
                 msg['To'] = target_email
                 html = """ <!DOCTYPE html>
                 <html>
                 <head></head>
                 <body>
-                <h1>failed to select mail inbox</h1>
+                <h1>Failed to select mail inbox</h1>
                 <br>
                 <p>Error: {e}</p>
                 </body>
@@ -126,27 +128,27 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Error Sent")
+                    print("INFO: Error Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: Failed to send email")
                 
 
             try:
                 # retrieve all emails  
                 status, data = mail.search(None, 'ALL')
             except Exception as e:
-                print("Error: failed to retrieve mails")
+                print("Error: Failed to retrieve mails")
                 msg = MIMEMultipart('alternative')
-                msg['Subject'] = "failed to retrieve mails"
+                msg['Subject'] = "Failed to retrieve mails"
                 msg['From'] = username
                 msg['To'] = target_email
                 html = """ <!DOCTYPE html>
                 <html>
                 <head></head>
                 <body>
-                <h1>failed to retrieve mails</h1>
+                <h1>Failed to retrieve mails</h1>
                 <br>
-                <p>Error: {e}</p>
+                <p>ERROR: {e}</p>
                 </body>
                 </html>""".format(e = e)
 
@@ -161,9 +163,9 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Error Sent")
+                    print("INFO: Error Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: Failed to send email")
                 
 
             mail_ids = []
@@ -202,10 +204,10 @@ def func():
                     
                             subject = mail_subject
                     
-            print("     Prompt: ", subject)
+            print("    Prompt: ", subject)
             print("")
             if subject == "help":
-                print("sending help message...")
+                print("INFO: Sending help message...")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "EPS32mail help"
                 msg['From'] = username
@@ -214,17 +216,17 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>EPS32mail help</h1>
+                <h1>Camera Mail Help</h1>
                 <br>
                 <h2>Options</h2>
-                <p>help : send help message</p>
-                <p>none : do nothing</p>
-                <p>cap : capture photo</p>
-                <p>status : camera status</p>
-                <p>100 : 100 seconds cycle delay</p>
-                <p>200 : 200 seconds cycle delay</p>
-                <p>3600 : 3600 seconds cycle delay</p>
-                <p>7200 : 7200 seconds cycle delay</p>
+                <p>help: send help message</p>
+                <p>none: do nothing</p>
+                <p>cap: capture photo</p>
+                <p>status: camera status</p>
+                <p>100: 100 seconds cycle delay</p>
+                <p>200: 200 seconds cycle delay</p>
+                <p>3600: 3600 seconds cycle delay</p>
+                <p>7200: 7200 seconds cycle delay</p>
                 </body>
                 </html>"""
 
@@ -239,16 +241,16 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
 
             if subject == "none":
-                print("Do Nothing...")
+                print("INFO: Do Nothing...")
 
             if subject == "100":
                 delay = 100
-                print("Delay set to 100 seconds...")
+                print("INFO: Delay set to 100 seconds...")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "100 seconds cycle delay"
                 msg['From'] = username
@@ -257,9 +259,9 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>100 second cycle delay</h1>
+                <h2>100 second cycle delay</h2>
                 <br>
-                <h2>set to 100 seconds cycle delay</h2>
+                <h2>Set to 100 seconds cycle delay</h2>
                 </body>
                 </html>"""
 
@@ -274,13 +276,13 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: Failed to send email")
 
             if subject == "200":
                 delay = 200
-                print("Delay set to 200 seconds...")
+                print("INFO: Delay set to 200 seconds...")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "200 seconds cycle delay"
                 msg['From'] = username
@@ -289,9 +291,9 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>200 second cycle delay</h1>
+                <h2>200 second cycle delay</h2>
                 <br>
-                <h2>set to 200 seconds cycle delay</h2>
+                <h2>Set to 200 seconds cycle delay</h2>
                 </body>
                 </html>"""
 
@@ -306,14 +308,14 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
 
     
             if subject == "3600":
                 delay = 3600
-                print("Delay set to 1 hour...")
+                print("INFO: Delay set to 1 hour...")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "1 hour cycle delay"
                 msg['From'] = username
@@ -322,7 +324,7 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>1 hour cycle delay</h1>
+                <h1>1 hour cycle delay</h>
                 <br>
                 <h2>set to 1 hour cycle delay</h2>
                 </body>
@@ -339,13 +341,13 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
     
             if subject == "7200":
                 delay = 7200
-                print("Delay set to 2 hours...")
+                print("INFO: Delay set to 2 hours...")
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "2 hours cycle delay"
                 msg['From'] = username
@@ -356,7 +358,7 @@ def func():
                 <body>
                 <h1>2 hours cycle delay</h1>
                 <br>
-                <h2>set to 2 hours cycle delay</h2>
+                <h2>Set to 2 hours cycle delay</h2>
                 </body>
                 </html>"""
 
@@ -371,28 +373,28 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
             
             if subject == "status":
-                print("Retrieving camera status...")
+                print("INFO: Retrieving camera status...")
                 try:
-                    cmd = "ping 192.168.1.106"
+                    cmd = "ping 10.110.169.10"
                     result = subprocess.check_output(cmd, shell=True, text=True)
                 except Exception as e:
-                    print("Error:", e)
+                    print("ERROR:", e)
                     result = e
             
                 msg = MIMEMultipart('alternative')
-                msg['Subject'] = "camera status"
+                msg['Subject'] = "Camera Status"
                 msg['From'] = username
                 msg['To'] = target_email
                 html = """ <!DOCTYPE html>
                 <html>
                 <head></head>
                 <body>
-                <h1>camera status</h1>
+                <h1>Camera Status</h1>
                 <br>
                 <p>{result}</p>
                 </body>
@@ -409,53 +411,72 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except:
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
 
             if subject == "cap":
-                print("Capturing image...")
-                current_datetime = datetime.now()
+                print("INFO: Capturing image...")
                 error_selenium = "NONE"
                 error_download = "NONE"
 
                 
-                options = webdriver.ChromeOptions()
+                options = Options()
                 options.add_argument('--headless')
                 options.add_argument("--disable-logging")  # Disables all logging
-                options.add_argument("--log-level=OFF")
+                options.add_argument("--log-level=3")
+                options.add_experimental_option('excludeSwitches', ['enable-logging'])
                 driver = webdriver.Chrome(options=options)
-                url = "http://192.168.1.106/"
+                url = "http://10.110.169.10/"
+                # click on capture button 
                 try:
                     driver.get(url)
                     element = driver.find_element(By.XPATH, "/html/body/div[1]/p[2]/button[2]")
                     element.click()
+                    print("")
+                    print("INFO: Selenium is clicked on capture button")
                 except Exception as e:
-                    print("Error: failed to start selenium")
+                    print("ERROR: Failed to start selenium")
                     error_selenium = e
 
                 sleep(6)
 
+                # download image
                 try:
-                    res = requests.get("http://192.168.1.106/saved-photo").content
+                    res = requests.get("http://10.110.169.10/saved-photo").content
                     f = open('saved-photo.jpg', 'wb')
                     f.write(res)
                     f.close()
+                    print("INFO: Image downloaded")
                 except Exception as e:
-                    print("Error: failed to download image")          
+                    print("ERROR: Failed to download image")          
                     error_download = e 
+
+                
 
                 today = date.today() 
 
+                current_datetime = datetime.now()
+                
+                # store hour an min in file name
+                hour = current_datetime.hour
+                min = current_datetime.minute
+                time = str(hour) + "," + str(min) + ".jpg"
 
+                # create folder
                 folder_name = str(today.month) + "." + str(today.day)
                 path = "image_archive/" + folder_name
-
                 exist = os.path.exists(path)
-                
                 if exist == False:
-                    os.makedirs(path)         
+                    print("INFO: folder craeted")
+                    os.makedirs(path) 
+                else: 
+                    print("INFO: folder exist")
+                
+                # rename image name
+                os.rename('saved-photo.jpg', str(time))
 
+        
                 msg = MIMEMultipart('alternative')
                 msg['Subject'] = "Captured image"
                 msg['From'] = username
@@ -464,7 +485,7 @@ def func():
                 <html>
                 <head></head>
                 <body>
-                <h1>Captured image</h1>
+                <h1>Captured Image</h1>
                 <p>Selenium_error: {error_selenium}</p>
                 <p>Download_error: {error_download}</p>
                 <p>Time: {current_datetime}</p>
@@ -474,13 +495,14 @@ def func():
                 text = MIMEText(html, 'html')
                 msg.attach(text)
 
+                # attach image to email
                 try:
-                    fp = open('saved-photo.jpg', 'rb')
+                    fp = open(str(time), 'rb')
                     image = MIMEImage(fp.read())
                     fp.close()
                     msg.attach(image)
                 except:
-                    print("Error: failed to find image")
+                    print("ERROR: failed to find image")
 
                 se_server = smtplib.SMTP('smtp.gmail.com', 587)
                 sleep(2)
@@ -491,9 +513,16 @@ def func():
                     se_server.sendmail(username, target_email, msg.as_string())
                     se_server.quit()
                     se_server.close()
-                    print("Email Sent")
+                    print("INFO: Email Sent")
                 except: 
-                    print("Error: failed to send email")
+                    print("ERROR: failed to send email")
+                
+                # move image to archive folder
+                source = str(time)
+                dest = path 
+                print("INFO: ", source)
+                shutil.move(source, dest)
+                print("INFO: image file moved to archive")
 
 
             print("===================================")
@@ -501,10 +530,9 @@ def func():
             sleep(delay)
 
         except Exception as e:
-            ii += 1
-            print("Main loop error:", e)
+            print("MAIN_LOOP_ERROR:", e)
             print("===================================")
-            sleep(3)
+            sleep(20)
             func()
     
 func()
